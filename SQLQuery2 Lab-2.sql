efa0a0bb99c615af00e054336dbf68533147ee79
Darshan1315 <149074746+Darshan1315@users.noreply.github.com>
2024-02-01 18:36:00 +0530

    SQLQuery2 Lab-2.sql

--- SQLQuery2 Lab-2.sql
+++ SQLQuery2 Lab-2.sql
@@ -14,8 +14,6 @@ values(104, 'Amity Multispecialist Hospital', 9012345678, 'Ravapar Road, Morbi',
 insert into Hospital_Detail(H_ID,H_Name,H_ContactNo,H_Address,H_City,H_Type,H_Fire_Saftey,H_Canteen_Facility)
 values(105, 'General Hospital', 7215698430, 'Jamnagar Highway, Rajkot', 'Rajkot', 'Govt. Hospital', 1, 1)
 
-select * from Hospital_Detail
-
 
 --table Room_Detail--
 
@@ -48,8 +46,6 @@ values(2009, 406, 'No', 'Non-AC', 1200)
 
 insert into Room_Detail(R_ID,R_Number,R_Availability,R_Type,R_Charge)
 values(2010, 801, 'Yes', 'General', 500)
-
-select * from Room_Detail
 
 --table Doctor_Detail--
 
@@ -84,7 +80,6 @@ values(3009, 'Anil', 'Ortho', 'M', 55000, 'anil@yahoo.in', 8765123490, 'Lal Darw
 insert into Doctor_Detail(D_ID,D_Name,D_Designation,D_Gender,D_Salary,D_EmailID,D_ContactNo,D_Address,D_City,H_ID)
 values(3010, 'Milan', 'MD', 'M', 45500, 'milan@gov.in', 9874563210, 'Near Green garden, Blocl 350', 'Rajkot', 105)
 
-select * from Doctor_Detail
 
 --table Appointment_Detail--
 
@@ -112,7 +107,6 @@ values(4007, 120, '2022-08-04', '10:35:20:004', 3009, 101)
 insert into Appointment_Detail(A_ID,A_Number,A_Date,A_Time,D_ID,H_ID)
 values(4008, 100, '2024-02-03', '16:45:00:000', 3002, 101)
 
-select * from Appointment_Detail
 
 --table Patient_Detail--'
 
@@ -146,53 +140,71 @@ values(5009, 'Mehul', 'M', 'Joshi', 7765124490, 20, 50.00, 'Taj road', 'Ahmedaba
 insert into Patient_Detail(P_ID,P_FirstName,P_MiddleName,P_LastName,P_ContactNo,P_Age,P_Weight,P_Address,P_City,P_Gender,P_Disease,P_Disease_Remarks,P_Guardian_ContactNo,A_ID,R_ID,D_ID,H_ID)
 values(5010, 'Priya', 'H', 'Parmar', 2597891999, 49, 76.25, 'Alap villa', 'Morbi', 'G', 'Migrain', 'Improve Sleep Habit', 9090891999, 4007, 2002, 3004, 103)
 
-select * from Patient_Detail
 
---table 6--
+--table Bill_Detail--
 
-insert into Bill_Detail(B_ID,P_ID,R_ID,H_ID,P_AdmitDateTime,P_DischargeDateTime,P_Medicine_Charge,P_TotalDays_Charge,P_Doctor_Charge,P_Total_Amount,P_Pending_Amount)
-values(6001,5001,2005,102,'2023-12-10 12:25:50:000','2023-12-11 12:25:50:000',3255.25,1500,500.25,5255.5,2500)
+insert into Bill_Detail(B_ID, P_ID, R_ID, H_ID, P_AdmitDateTime, P_DischargeDateTime, P_Medicine_Charge, P_TotalDays_Charge, P_Doctor_Charge, P_Total_Amount, P_Pending_Amount)
+values(6001, 5001, 2005, 102, '10-12-2023 12:25:50:000', '11-12-2023 12:25:50:000', 3255.25, 1500, 500.25, 5255.2, 2500)
 
-insert into Bill_Detail(B_ID,P_ID,R_ID,H_ID,P_AdmitDateTime,P_DischargeDateTime,P_Medicine_Charge,P_TotalDays_Charge,P_Doctor_Charge,P_Total_Amount,P_Pending_Amount)
-values(6002,5002,2001,101,'2023-11-17 17:02:10:002','2023-11-20 17:02:10:002',5600,6000,1050,12650,1000.)
+insert into Bill_Detail(B_ID, P_ID, R_ID, H_ID, P_AdmitDateTime, P_DischargeDateTime, P_Medicine_Charge, P_TotalDays_Charge, P_Doctor_Charge, P_Total_Amount, P_Pending_Amount)
+values(6002, 5002, 2001, 101, '11-11-2023 11:02:10:002', '11-11-2023 11:02:10:002', 5600, 6000, 1050, 12650, 1000)
 
-insert into Bill_Detail(B_ID,P_ID,R_ID,H_ID,P_AdmitDateTime,P_DischargeDateTime,P_Medicine_Charge,P_TotalDays_Charge,P_Doctor_Charge,P_Total_Amount,P_Pending_Amount)
-values(6003,5003,2002,103,'2024-01-15 17:30:30:030','2024-01-25 17:30:30:030',100.56,15000,820,15920.56,0)
+insert into Bill_Detail(B_ID, P_ID, R_ID, H_ID, P_AdmitDateTime, P_DischargeDateTime, P_Medicine_Charge, P_TotalDays_Charge, P_Doctor_Charge, P_Total_Amount, P_Pending_Amount)
+values(6003, 5003, 2002, 103, '01-15-2024 17:30:30:030', '01-25-2024 17:30:30:030', 100.56, 15000, 820, 15920.56, 0)
 
-insert into Bill_Detail(B_ID,P_ID,R_ID,H_ID,P_AdmitDateTime,P_DischargeDateTime,P_Medicine_Charge,P_TotalDays_Charge,P_Doctor_Charge,P_Total_Amount,P_Pending_Amount)
-values(6004,5004,2004,103,'2022-01-10 11:11:00:020','2022-01-16 11:11:00:020',1005.2,18000,720.56,19725.76,2500.25)
+insert into Bill_Detail(B_ID, P_ID, R_ID, H_ID, P_AdmitDateTime, P_DischargeDateTime, P_Medicine_Charge, P_TotalDays_Charge, P_Doctor_Charge, P_Total_Amount, P_Pending_Amount)
+values(6004, 5004, 2004, 104, '10-01-2022 11:11:00:020', '01-16-2022 11:11:00:020', 1005.2, 18000, 720.56, 19725.76, 2500.25)
 
-insert into Bill_Detail(B_ID,P_ID,R_ID,H_ID,P_AdmitDateTime,P_DischargeDateTime,P_Medicine_Charge,P_TotalDays_Charge,P_Doctor_Charge,P_Total_Amount,P_Pending_Amount)
-values(6005,5005,2001,101,'2023-12-25 11:30:45:001','2023-12-30 11:30:45:001',752,10000,1500,12254,4000.6)
+insert into Bill_Detail(B_ID, P_ID, R_ID, H_ID, P_AdmitDateTime, P_DischargeDateTime, P_Medicine_Charge, P_TotalDays_Charge, P_Doctor_Charge, P_Total_Amount, P_Pending_Amount)
+values(6005, 5005, 2001, 101, '12-25-2023 11:30:45:001', '12-30-2023 11:30:45:001', 754, 10000, 1500, 12254, 4000.6)
 
-insert into Bill_Detail(B_ID,P_ID,R_ID,H_ID,P_AdmitDateTime,P_DischargeDateTime,P_Medicine_Charge,P_TotalDays_Charge,P_Doctor_Charge,P_Total_Amount,P_Pending_Amount)
-values(6006,5006,2001,101,'2021-02-22 14:15:20:004','2021-02-24 14:15:20:004',6000,4000,320,10320,0)
+insert into Bill_Detail(B_ID, P_ID, R_ID, H_ID, P_AdmitDateTime, P_DischargeDateTime, P_Medicine_Charge, P_TotalDays_Charge, P_Doctor_Charge, P_Total_Amount, P_Pending_Amount)
+values(6006, 5006, 2001, 101, '02-22-2021 14:15:20:004', '02-24-2021 14:15:20:005', 6000, 4000, 320, 10320, 0)
 
-insert into Bill_Detail(B_ID,P_ID,R_ID,H_ID,P_AdmitDateTime,P_DischargeDateTime,P_Medicine_Charge,P_TotalDays_Charge,P_Doctor_Charge,P_Total_Amount,P_Pending_Amount)
-values(6007,5007,2006,104,'2024-02-07 10:45:00:000','2024-02-10 10:45:00:000',4570,13500,600.35,18670.35,6000)
+insert into Bill_Detail(B_ID, P_ID, R_ID, H_ID, P_AdmitDateTime, P_DischargeDateTime, P_Medicine_Charge, P_TotalDays_Charge, P_Doctor_Charge, P_Total_Amount, P_Pending_Amount)
+values(6007, 5007, 2006, 104, '07-02-2024 10:45:00:000', '10-02-2024 10:45:00:000', 4570, 13500, 600.35, 18670.35, 6000)
 
-insert into Bill_Detail(B_ID,P_ID,R_ID,H_ID,P_AdmitDateTime,P_DischargeDateTime,P_Medicine_Charge,P_TotalDays_Charge,P_Doctor_Charge,P_Total_Amount,P_Pending_Amount)
-values(6006,5008,2010,101,'2023-04-21 13:30:20:000','2023-04-27 13:30:20:000',1600.45,3000,250,4850.45,1500.71)
+insert into Bill_Detail(B_ID, P_ID, R_ID, H_ID, P_AdmitDateTime, P_DischargeDateTime, P_Medicine_Charge, P_TotalDays_Charge, P_Doctor_Charge, P_Total_Amount, P_Pending_Amount)
+values(6008, 5008, 2010, 101, '04-21-2023 13:30:20:000', '04-27-2023 13:30:20:000', 1600.45, 3000, 250, 4850.45, 1500.71)
 
-insert into Bill_Detail(B_ID,P_ID,R_ID,H_ID,P_AdmitDateTime,P_DischargeDateTime,P_Medicine_Charge,P_TotalDays_Charge,P_Doctor_Charge,P_Total_Amount,P_Pending_Amount)
-values(6009,5009,2003,104,'2022-08-04 10:45:20:004','2022-08-13 10:45:20:004',2600,40500,2000,45100,5000)
+insert into Bill_Detail(B_ID, P_ID, R_ID, H_ID, P_AdmitDateTime, P_DischargeDateTime, P_Medicine_Charge, P_TotalDays_Charge, P_Doctor_Charge, P_Total_Amount, P_Pending_Amount)
+values(6009, 5009, 2003, 104, '04-08-2022 10:45:20:004', '08-13-2022 10:45:20:004', 2600, 40500, 2000, 45100, 5000)
 
-insert into Bill_Detail(B_ID,P_ID,R_ID,H_ID,P_AdmitDateTime,P_DischargeDateTime,P_Medicine_Charge,P_TotalDays_Charge,P_Doctor_Charge,P_Total_Amount,P_Pending_Amount)
-values(6010,5010,2002,103,'2024-02-04 18:25:10:001','2024-02-06 18:25:10:001',3500,3000,400,6900,3500)
+insert into Bill_Detail(B_ID, P_ID, R_ID, H_ID, P_AdmitDateTime, P_DischargeDateTime, P_Medicine_Charge, P_TotalDays_Charge, P_Doctor_Charge, P_Total_Amount, P_Pending_Amount)
+values(6010, 5010, 2002, 103, '04-02-2024 18:25:10:001', '06-02-2024 18:25:10:001', 3500, 3000, 400, 6900, 3500)
 
-select * from Bill_Detail
 
-insert into Transaction_Detail(Tr_ID,Tr_Number,Tr_B_Amout,Tr_DateTime,Tr_Type,B_ID,P_ID,H_ID)
-values(7001,100021,2755.5,'2023-12-11 12:25:50:000','Cheque',6001,5001,102)
+--table Transaction_Detail--
 
-insert into Transaction_Detail(Tr_ID,Tr_Number,Tr_B_Amout,Tr_DateTime,Tr_Type,B_ID,P_ID,H_ID)
-values(7001,100021,2755.5,'2023-12-11 12:25:50:000','Cheque',6001,5001,102)
+insert into Transaction_Detail(Tr_ID, Tr_Number, Tr_B_Amount, Tr_DateTime, Tr_Type, B_ID, P_ID, H_ID)
+values(7001, 100021, 2755.5, '11-12-2023 12:25:50:000', 'Cheque', 6001, 5001, 102)
 
-insert into Transaction_Detail(Tr_ID,Tr_Number,Tr_B_Amout,Tr_DateTime,Tr_Type,B_ID,P_ID,H_ID)
-values(7001,100021,2755.5,'2023-12-11 12:25:50:000','Cheque',6001,5001,102)
+insert into Transaction_Detail(Tr_ID, Tr_Number, Tr_B_Amount, Tr_DateTime, Tr_Type, B_ID, P_ID, H_ID)
+values(7002, NULL, 11650, NULL, 'Cash', 6002, 5002, 101)
 
-insert into Transaction_Detail(Tr_ID,Tr_Number,Tr_B_Amout,Tr_DateTime,Tr_Type,B_ID,P_ID,H_ID)
-values(7001,100021,2755.5,'2023-12-11 12:25:50:000','Cheque',6001,5001,102)
+insert into Transaction_Detail(Tr_ID, Tr_Number, Tr_B_Amount, Tr_DateTime, Tr_Type, B_ID, P_ID, H_ID)
+values(7003, 'F14O2RD', 15920.56, '01-25-2024 17:30:30:030', 'Online', 6003, 5003, 103)
 
-insert into Transaction_Detail(Tr_ID,Tr_Number,Tr_B_Amout,Tr_DateTime,Tr_Type,B_ID,P_ID,H_ID)
-values(7001,100021,2755.5,'2023-12-11 12:25:50:000','Cheque',6001,5001,102)
+insert into Transaction_Detail(Tr_ID, Tr_Number, Tr_B_Amount, Tr_DateTime, Tr_Type, B_ID, P_ID, H_ID)
+values(7004, 250001, 17225.51, '01-15-2022 11:11:00:020', 'Cheque', 6004, 5004, 103)
+
+insert into Transaction_Detail(Tr_ID, Tr_Number, Tr_B_Amount, Tr_DateTime, Tr_Type, B_ID, P_ID, H_ID)
+values(7005, 100001, 8253.4, '12-27-2023 11:30:05:001', 'Cheque', 6005, 5005, 101)
+
+insert into Transaction_Detail(Tr_ID, Tr_Number, Tr_B_Amount, Tr_DateTime, Tr_Type, B_ID, P_ID, H_ID)
+values(7006, 'eQ632aB', 10320, '02-24-2021 14:15:20:004', 'Online', 6006, 5006, 101)
+
+insert into Transaction_Detail(Tr_ID, Tr_Number, Tr_B_Amount, Tr_DateTime, Tr_Type, B_ID, P_ID, H_ID)
+values(7007, NULL, 12670.35, NULL, 'Cash', 6007, 5007, 104)
+
+insert into Transaction_Detail(Tr_ID, Tr_Number, Tr_B_Amount, Tr_DateTime, Tr_Type, B_ID, P_ID, H_ID)
+values(7008, 'KHT98AJ', 3349.74, '04-27-2023 10:30:20:000', 'Online', 6008, 5008, 101)
+
+insert into Transaction_Detail(Tr_ID, Tr_Number, Tr_B_Amount, Tr_DateTime, Tr_Type, B_ID, P_ID, H_ID)
+values(7009, 500006, 40100, '12-08-2022 10:45:20:004', 'Cheque', 6009, 5009, 104)
+
+insert into Transaction_Detail(Tr_ID, Tr_Number, Tr_B_Amount, Tr_DateTime, Tr_Type, B_ID, P_ID, H_ID)
+values(7010, 'Xze57CK', 3400, '07-02-2024 18:25:10:001', 'Online', 6010, 5010, 103)
+
+
+select * from Transaction_Detail
