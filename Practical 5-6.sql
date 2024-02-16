6bef3dd870f0f7757a3af737da167ff0c46e48d7
Darshan1315 <149074746+Darshan1315@users.noreply.github.com>
2024-02-16 10:53:12 +0530

    Practical 5-6.sql

--- Practical 5-6.sql
+++ Practical 5-6.sql
@@ -31,3 +31,65 @@ select max(D_Salary)as Maximum_Salary,
        avg(D_Salary) as Avarage_Salary
 from Doctor_Detail
 
+---p6---
+---1--
+
+select R_Type ,count(R_Type)
+from Room_Detail 
+group by R_Type
+
+--2--
+
+select P_City,count(P_ID)
+from Patient_Detail 
+group by P_City
+
+--3--
+select H_Canteen_Facility,count(H_Canteen_Facility)
+from Hospital_Detail 
+group by H_Canteen_Facility
+
+--4--
+
+select P_Disease,count(P_Disease)
+from Patient_Detail 
+group by P_Disease
+
+--5---
+
+select D_Designation,count(D_Designation)
+from Doctor_Detail where D_Designation='Dentist' 
+group by D_Designation
+
+---6--
+
+select Tr_Type,count(Tr_Type)
+from Transaction_Master 
+group by Tr_Type
+
+--7--
+
+select H_Name
+from Hospital_Detail   
+order by H_Name desc
+---8---
+select D_Name
+from Doctor_Detail   
+order by D_Name asc
+
+--9--
+select D_Name,D_EmailID,H_ID,D_Salary
+from Doctor_Detail 
+order by D_Salary desc
+
+---10---
+
+select R_Type,R_Charge
+from Room_Detail
+order by R_Charge
+
+--11--
+
+select P_FirstName,P_Age
+from Patient_Detail 
+order by P_Age desc
